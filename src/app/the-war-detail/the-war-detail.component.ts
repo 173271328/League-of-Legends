d9864b9ac65eeef09c608c45421b32d56509a0b9
173271328 <liugaofeng5178@163.com>
2017-10-27 16:37:44 +0800

    比赛详情

--- src/app/the-war-detail/the-war-detail.component.ts
+++ src/app/the-war-detail/the-war-detail.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HeaderNameService } from '../headerchange/header-name.service'
 @Component({
-  selector: 'app-the-war-detail',
-  templateUrl: './the-war-detail.component.html',
-  styleUrls: ['./the-war-detail.component.css']
+    selector: 'app-the-war-detail',
+    templateUrl: './the-war-detail.component.html',
+    styleUrls: ['./the-war-detail.component.css']
 })
 export class TheWarDetailComponent implements OnInit {
+    title: string = '对战详情'
+    constructor(service: HeaderNameService) {
+        service.change.emit(this.title)
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
